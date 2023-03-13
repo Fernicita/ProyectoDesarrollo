67a18c65b176b6a00e1ae03885d32c47ee11650c
manuel calleros <jesusmanuelcallerosvazquez@hotmail.com>
2023-03-13 13:34:01 -0600

    Agregadas funciones
    Se implementan las funciones de agregar, buscar y leer. Se cambia el menu de la seccion -a

--- app.sh
+++ app.sh
@@ -2,6 +2,27 @@
 
 sectiona=(0 SCRUM X.P. Kanban Crystal)
 sectiont=(0 Cascada Espiral ModeloV)
+
+function agregar() {
+  read -p "Ingrese el identificador del concepto: " concepto
+  read -p "Ingrese la definición del concepto: " definicion
+  echo "[$concepto] .- $definicion" >> "$1"
+}
+
+function buscar() {
+  read -p "Ingrese el concepto a buscar: " buscar
+  grep "\[$buscar\]" "$1"
+}
+
+
+function eliminar() {
+  echo
+}
+
+function leer() {
+  cat "$1"
+}
+
 case $1 in
   -a)
   echo "Bienvenido a la guia rapida de Agile"
@@ -13,42 +34,59 @@ case $1 in
   case $x in
     1)
     echo "Usted esta en la seccion ${sectiona[$x]}"
-    echo "1. Agregar informacion"
-    echo "2. Buscar"
-    echo "3. Eliminar informacion"
-    echo "4. Leer base de informacion"
-    read -p "Selecione la opcion que desea utilizar" opc
-
+    archivo="SCRUM.inf"
     ;;
     2)
     echo "Usted esta en la seccion ${sectiona[$x]}"
-    echo "1. Agregar informacion"
-    echo "2. Buscar"
-    echo "3. Eliminar informacion"
-    echo "4. Leer base de informacion"
-    read -p "Selecione la opcion que desea utilizar" opc
+    archivo="XP.inf"
     ;;
     3)
     echo "Usted esta en la seccion ${sectiona[$x]}"
-    echo "1. Agregar informacion"
-    echo "2. Buscar"
-    echo "3. Eliminar informacion"
-    echo "4. Leer base de informacion"
-    read -p "Selecione la opcion que desea utilizar" opc
+    archivo="Kanban.inf"
     ;;
     4)
     echo "Usted esta en la seccion ${sectiona[$x]}"
-    echo "1. Agregar informacion"
-    echo "2. Buscar"
-    echo "3. Eliminar informacion"
-    echo "4. Leer base de informacion"
-    read -p "Selecione la opcion que desea utilizar" opc
+    archivo="Crystal.inf"
     ;;
     *)
     echo "Opcion no valida"
     ;;
   esac
+
+  if [ -n "$archivo" ]; then
+    while true; do
+      echo "1. Agregar informacion"
+      echo "2. Buscar"
+      echo "3. Eliminar informacion"
+      echo "4. Leer base de informacion"
+      read -p "Seleccione la opcion que desea utilizar: " opc
+
+      case $opc in
+        1)
+        agregar "$archivo"
+        ;;
+        2)
+        buscar "$archivo"
+        ;;
+        3)
+        eliminar "$archivo"
+        ;;
+        4)
+        leer "$archivo"
+        ;;
+        *)
+        echo "Opcion no valida"
+        ;;
+      esac
+
+      read -p "Desea continuar en esta seccion? (s/n): " continuar
+      if [ "$continuar" != "s" ]; then
+        break
+      fi
+    done
+  fi
   ;;
+  
   -t)
   echo "Bienvenido a la guia rapida de Metodologias Tradicionales"
   echo "1. Cascada"
