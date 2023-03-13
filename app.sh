84dd9b4ce1dee08d621d6e1c306f770f431c7b06
RamonPloP <aspirelive02@gmail.com>
2023-03-13 14:44:18 -0600

    Se agrego el menu para seguir o salir de seccion o programa

--- app.sh
+++ app.sh
@@ -25,104 +25,136 @@ function leer() {
 
 case $1 in
   -a)
-  echo "Bienvenido a la guia rapida de Agile"
-  echo "1. SCRUM"
-  echo "2. X.P."
-  echo "3. Kanban"
-  echo "4. Crystal"
-  read -p "Para continuar seleccione un tema: " x
-  case $x in
-    1)
-    echo "Usted esta en la seccion ${sectiona[$x]}"
-    archivo="SCRUM.inf"
-    ;;
-    2)
-    echo "Usted esta en la seccion ${sectiona[$x]}"
-    archivo="XP.inf"
-    ;;
-    3)
-    echo "Usted esta en la seccion ${sectiona[$x]}"
-    archivo="Kanban.inf"
-    ;;
-    4)
-    echo "Usted esta en la seccion ${sectiona[$x]}"
-    archivo="Crystal.inf"
-    ;;
-    *)
-    echo "Opcion no valida"
-    ;;
-  esac
-
-  if [ -n "$archivo" ]; then
     while true; do
-      echo "1. Agregar informacion"
-      echo "2. Buscar"
-      echo "3. Eliminar informacion"
-      echo "4. Leer base de informacion"
-      read -p "Seleccione la opcion que desea utilizar: " opc
-
-      case $opc in
+      echo "Bienvenido a la guia rapida de Agile"
+      echo "1. SCRUM"
+      echo "2. X.P."
+      echo "3. Kanban"
+      echo "4. Crystal"
+      read -p "Para continuar seleccione un tema: " x
+      case $x in
         1)
-        agregar "$archivo"
+        echo "Usted esta en la seccion ${sectiona[$x]}"
+        archivo="SCRUM.inf"
         ;;
         2)
-        buscar "$archivo"
+        echo "Usted esta en la seccion ${sectiona[$x]}"
+        archivo="XP.inf"
         ;;
         3)
-        eliminar "$archivo"
+        echo "Usted esta en la seccion ${sectiona[$x]}"
+        archivo="Kanban.inf"
         ;;
         4)
-        leer "$archivo"
+        echo "Usted esta en la seccion ${sectiona[$x]}"
+        archivo="Crystal.inf"
         ;;
         *)
         echo "Opcion no valida"
         ;;
       esac
 
-      read -p "Desea continuar en esta seccion? (s/n): " continuar
+      if [ -n "$archivo" ]; then
+        while true; do
+          echo "1. Agregar informacion"
+          echo "2. Buscar"
+          echo "3. Eliminar informacion"
+          echo "4. Leer base de informacion"
+          read -p "Seleccione la opcion que desea utilizar: " opc
+
+          case $opc in
+            1)
+            agregar "$archivo"
+            ;;
+            2)
+            buscar "$archivo"
+            ;;
+            3)
+            eliminar "$archivo"
+            ;;
+            4)
+            leer "$archivo"
+            ;;
+            *)
+            echo "Opcion no valida"
+            ;;
+          esac
+
+          read -p "Desea continuar en esta seccion? (s/n): " continuar
+          if [ "$continuar" != "s" ]; then
+            break
+          fi
+        done
+      fi
+      read -p "Quieres seguir en el programa? (s/n): " continuar
       if [ "$continuar" != "s" ]; then
         break
       fi
-    done
-  fi
-  ;;
-  
+      done
+      ;;
+
   -t)
-  echo "Bienvenido a la guia rapida de Metodologias Tradicionales"
-  echo "1. Cascada"
-  echo "2. Espiral"
-  echo "3. Modelo V"
-  read -p "Para continuar seleccione un tema: " x
-  case $x in
-    1)
-    echo "Usted esta en la seccion ${sectiont[$x]}"
-    echo "1. Agregar informacion"
-    echo "2. Buscar"
-    echo "3. Eliminar informacion"
-    echo "4. Leer base de informacion"
-    read -p "Selecione la opcion que desea utilizar" opc
-    ;;
-    2)
-    echo "Usted esta en la seccion ${sectiont[$x]}"
-    echo "1. Agregar informacion"
-    echo "2. Buscar"
-    echo "3. Eliminar informacion"
-    echo "4. Leer base de informacion"
-    read -p "Selecione la opcion que desea utilizar" opc
-    ;;
-    3)
-    echo "Usted esta en la seccion ${sectiont[$x]}"
-    echo "1. Agregar informacion"
-    echo "2. Buscar"
-    echo "3. Eliminar informacion"
-    echo "4. Leer base de informacion"
-    read -p "Selecione la opcion que desea utilizar" opc
-    ;;
-    *)
-    echo "Opcion no valida"
-    ;;
-  esac
-  ;;
+  while true; do
+    echo "Bienvenido a la guia rapida de Metodologias Tradicionales"
+    echo "1. Cascada"
+    echo "2. Espiral"
+    echo "3. Modelo V"
+    read -p "Para continuar seleccione un tema: " x
+      case $x in
+        1)
+        echo "Usted esta en la seccion ${sectiont[$x]}"
+        archivo="Cascada.inf"
+        ;;
+        2)
+        echo "Usted esta en la seccion ${sectiont[$x]}"
+        archivo="Espiral.inf"
+        ;;
+        3)
+        echo "Usted esta en la seccion ${sectiont[$x]}"
+        archivo="ModeloV.inf"
+        ;;
+        *)
+        echo "Opcion no valida"
+        ;;
+      esac
+      if [ -n "$archivo" ]; then
+        while true; do
+          echo "1. Agregar informacion"
+          echo "2. Buscar"
+          echo "3. Eliminar informacion"
+          echo "4. Leer base de informacion"
+          read -p "Seleccione la opcion que desea utilizar: " opc
+
+          case $opc in
+            1)
+            agregar "$archivo"
+            ;;
+            2)
+            buscar "$archivo"
+            ;;
+            3)
+            eliminar "$archivo"
+            ;;
+            4)
+            leer "$archivo"
+            ;;
+            *)
+            echo "Opcion no valida"
+            ;;
+          esac
+
+          read -p "Desea continuar en esta seccion? (s/n): " continuar
+          if [ "$continuar" != "s" ]; then
+            break
+          fi
+        done
+      fi
+      read -p "Quieres seguir en el programa? (s/n): " continuar
+      if [ "$continuar" != "s" ]; then
+        break
+      fi
+      done
+      ;;
   *)
   echo "Flag no encontrada"
   ;;
